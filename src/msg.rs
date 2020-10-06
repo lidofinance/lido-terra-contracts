ef49efcd19b3c46061df02e4cd055cbe71aa1664
MSNTCS <mohammad@terra.money>
2020-10-06 18:01:53 +0900

    Validate token info in init message

--- src/msg.rs
+++ src/msg.rs
@@ -1,4 +1,4 @@
-use cosmwasm_std::{HumanAddr, Uint128};
+use cosmwasm_std::{HumanAddr, StdError, StdResult, Uint128};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
@@ -7,6 +7,47 @@ pub struct InitMsg {
     pub name: String,
     pub symbol: String,
     pub decimals: u8,
+}
+
+impl InitMsg {
+    pub fn validate(&self) -> StdResult<()> {
+        // Check name, symbol, decimals
+        if !is_valid_name(&self.name) {
+            return Err(StdError::generic_err(
+                "Name is not in the expected format (3-30 UTF-8 bytes)",
+            ));
+        }
+        if !is_valid_symbol(&self.symbol) {
+            return Err(StdError::generic_err(
+                "Ticker symbol is not in expected format [A-Z]{3,6}",
+            ));
+        }
+        if self.decimals > 18 {
+            return Err(StdError::generic_err("Decimals must not exceed 18"));
+        }
+        Ok(())
+    }
+}
+
+fn is_valid_name(name: &str) -> bool {
+    let bytes = name.as_bytes();
+    if bytes.len() < 3 || bytes.len() > 30 {
+        return false;
+    }
+    true
+}
+
+fn is_valid_symbol(symbol: &str) -> bool {
+    let bytes = symbol.as_bytes();
+    if bytes.len() < 3 || bytes.len() > 6 {
+        return false;
+    }
+    for byte in bytes.iter() {
+        if *byte < 65 || *byte > 90 {
+            return false;
+        }
+    }
+    true
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
