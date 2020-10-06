4acba286098d494d1a827d57cbb10235e11718bc
MSNTCS <mohammad@terra.money>
2020-10-06 19:19:03 +0900

    Implement Mint message

--- src/contract.rs
+++ src/contract.rs
@@ -1,10 +1,11 @@
 use cosmwasm_std::{
-    Api, Env, Extern, HandleResponse, InitResponse, Querier, StdError,
+    log, Api, Env, Extern, HandleResponse, HumanAddr, InitResponse, Querier, StakingMsg, StdError,
     StdResult, Storage, Uint128,
 };
 
 use crate::msg::{HandleMsg, InitMsg};
-use crate::state::{token_info, TokenInfo};
+use crate::state::{balances, token_info, token_info_read, TokenInfo};
+use std::ops::Add;
 
 pub fn init<S: Storage, A: Api, Q: Querier>(
     deps: &mut Extern<S, A, Q>,
@@ -31,5 +32,61 @@ pub fn handle<S: Storage, A: Api, Q: Querier>(
     env: Env,
     msg: HandleMsg,
 ) -> StdResult<HandleResponse> {
-    unimplemented!()
+    match msg {
+        HandleMsg::Mint { validator, amount } => handle_mint(deps, env, validator, amount),
+        _ => Ok(HandleResponse::default()),
+    }
+}
+
+pub fn handle_mint<S: Storage, A: Api, Q: Querier>(
+    deps: &mut Extern<S, A, Q>,
+    env: Env,
+    validator: HumanAddr,
+    amount: Uint128,
+) -> StdResult<HandleResponse> {
+    if amount == Uint128::zero() {
+        return Err(StdError::generic_err("Invalid zero amount"));
+    }
+
+    let mut token = token_info_read(&deps.storage).load()?;
+
+    let payment = env
+        .message
+        .sent_funds
+        .iter()
+        .find(|x| x.denom == token.name)
+        .ok_or_else(|| StdError::generic_err(format!("No {} tokens sent", &token.name)))?;
+
+    let added_amount = payment.amount.add(amount);
+
+    token.total_supply += amount;
+
+    token_info(&mut deps.storage).save(&token)?;
+
+    let mut sub_env = env.clone();
+    sub_env.message.sender = env.contract.address.clone();
+
+    // Issue the bluna token for sender
+    let sender = sub_env.message.sender.clone();
+    let rcpt_raw = deps.api.canonical_address(&sender)?;
+    balances(&mut deps.storage).update(rcpt_raw.as_slice(), |balance: Option<Uint128>| {
+        Ok(balance.unwrap_or_default() + amount)
+    })?;
+
+    // bond them to the validator
+    let res = HandleResponse {
+        messages: vec![StakingMsg::Delegate {
+            validator,
+            amount: payment.clone(),
+        }
+        .into()],
+        log: vec![
+            log("action", "mint"),
+            log("from", env.message.sender),
+            log("bonded", payment.amount),
+            log("minted", added_amount),
+        ],
+        data: None,
+    };
+    Ok(res)
 }
