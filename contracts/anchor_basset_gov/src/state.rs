a2b0cd430683f93d964e50a9488558e3c2b8a9da
MSNTCS <najafi@codechain.io>
2020-10-19 20:34:16 +0900

    Complete the state

--- contracts/anchor_basset_gov/src/state.rs
+++ contracts/anchor_basset_gov/src/state.rs
@@ -1,12 +1,17 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
-use serde::ser::{SerializeMap, Serializer};
 
-use cosmwasm_std::{CanonicalAddr, Decimal, HumanAddr, ReadonlyStorage, StdError, Storage, Uint128, StdResult, from_slice};
-use cosmwasm_storage::{bucket, bucket_read, singleton, singleton_read, Bucket, ReadonlyBucket, ReadonlySingleton, Singleton, PrefixedStorage, ReadonlyPrefixedStorage};
+use cosmwasm_std::{
+    from_slice, to_vec, CanonicalAddr, Decimal, HumanAddr, Order, ReadonlyStorage, StdError,
+    StdResult, Storage, Uint128,
+};
+use cosmwasm_storage::{
+    bucket, bucket_read, singleton, singleton_read, Bucket, PrefixedStorage, ReadonlyBucket,
+    ReadonlyPrefixedStorage, ReadonlySingleton, Singleton,
+};
 use rand::Rng;
+use std::collections::hash_map::RandomState;
 use std::collections::HashMap;
-use cosmwasm_vm::to_vec;
 
 // EPOC = 21600s is equal to 6 hours
 pub const EPOC: u64 = 21600;
@@ -19,11 +24,13 @@ pub static POOL_INFO: &[u8] = b"pool_info";
 const BALANCE: &[u8] = b"balance";
 static PREFIX_REWARD: &[u8] = b"claim";
 
-pub static PREFIX_UNBOUND_PER_EPOC:&[u8]= b"unbound";
-pub static PREFIX_DELEGATION_MAP:&[u8] = b"delegate";
-pub static PREFIX_HOLDER_MAP:&[u8] = b"holder";
-pub static PREFIX_WAIT_MAP : &[u8] = b"wait";
-
+pub static PREFIX_UNBOUND_PER_EPOC: &[u8] = b"unbound";
+pub static PREFIX_DELEGATION_MAP: &[u8] = b"delegate";
+pub static PREFIX_HOLDER_MAP: &[u8] = b"holder";
+pub static PREFIX_WAIT_MAP: &[u8] = b"wait";
+pub static EPOC_ID: &[u8] = b"epoc";
+pub static VALIDATORS: &[u8] = b"validators";
+pub static ALL_EPOC_ID: &[u8] = b"epoc_list";
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct TokenInfo {
@@ -39,118 +46,32 @@ pub struct TokenInfo {
 )]
 pub struct EpocId {
     pub epoc_id: u64,
-}
-
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema, Default)]
-pub struct TokenState {
-    pub current_epoc: u64,
     pub current_block_time: u64,
-    pub delegation_map: HashMap<HumanAddr, Uint128>,
-    pub holder_map: HashMap<HumanAddr, Decimal>,
 }
 
-pub struct UndelegatedList{
-    pub undelegated_wait_list: HashMap<EpocId, Undelegation>,
-}
-
-impl TokenState {
+impl EpocId {
     pub fn compute_current_epoc(&mut self, block_time: u64) {
-        let epoc = self.current_epoc;
+        let epoc = self.epoc_id;
         let time = self.current_block_time;
 
         self.current_block_time = block_time;
-        self.current_epoc = epoc + (block_time - time) / EPOC;
+        self.epoc_id = epoc + (block_time - time) / EPOC;
     }
 
-    pub fn is_epoc_passed(&mut self, block_time: u64) -> bool {
+    pub fn is_epoc_passed(&self, block_time: u64) -> bool {
         let time = self.current_block_time;
 
-        self.current_block_time = block_time;
-        if (block_time - time) / EPOC < 1 {
+        if (block_time - time) > EPOC {
             return false;
         }
         true
     }
-
-    pub fn choose_validator(&self, claim: Uint128) -> HumanAddr {
-        let mut validator_array: Vec<HumanAddr> = Vec::new();
-        for (key, _) in self.delegation_map.iter() {
-            validator_array.push(HumanAddr::from(key));
-        }
-        let mut rng = rand::thread_rng();
-        loop {
-            let random = rng.gen_range(0, validator_array.capacity() - 1);
-            let address = validator_array.get(random).unwrap();
-            let address_clone = address.clone();
-            let val = self
-                .delegation_map
-                .get(address)
-                .expect("The address existence is checked previously");
-            if val > &claim {
-                return address_clone;
-            }
-        }
-    }
-
-    pub fn is_valid_address(&self, address: &HumanAddr) -> bool {
-        for (_, val) in self.undelegated_wait_list.iter() {
-            if val.undelegated_wait_list_map.contains_key(address) {
-                return true;
-            }
-        }
-        false
-    }
-
-    pub fn get_user_delegation_amount(
-        &self,
-        address: &HumanAddr,
-        epoc_id: &EpocId,
-    ) -> Result<&Uint128, StdError> {
-        let undelegated = self.undelegated_wait_list.get(epoc_id).unwrap();
-        if undelegated.is_address_exist(address) {
-            Ok(undelegated.undelegated_wait_list_map.get(address).unwrap())
-        } else {
-            return Err(StdError::generic_err(
-                "There is no record for user's delegation",
-            ));
-        }
-    }
-
-    pub fn set_new_delegation(&mut self, address: HumanAddr, epoc_id: &EpocId, amount: Uint128) {
-        let user_max = self.get_user_delegation_amount(&address, epoc_id).unwrap();
-        let decrease = user_max.0 - &amount.0;
-        if decrease != 0 {
-            let undelegated = self.undelegated_wait_list.get_mut(epoc_id).unwrap();
-            undelegated
-                .undelegated_wait_list_map
-                .insert(address, Uint128(decrease))
-                .expect("The existence of the address is checked before");
-        } else {
-            let undelegated = self.undelegated_wait_list.get_mut(epoc_id).unwrap();
-            undelegated.undelegated_wait_list_map.remove(&address);
-        }
-    }
 }
 
+//keep all unprocessed epocs
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema, Default)]
-pub struct Undelegation {
-    pub claim: Uint128,
-    pub undelegated_wait_list_map: HashMap<HumanAddr, Uint128>,
-}
-
-impl Undelegation {
-    pub fn compute_claim(&mut self) {
-        let mut claim = self.claim;
-        for (_, value) in self.undelegated_wait_list_map.iter() {
-            claim += *value;
-        }
-
-        self.claim = claim;
-    }
-
-    pub fn is_address_exist(&self, address: &HumanAddr) -> bool {
-        self.undelegated_wait_list_map.contains_key(address)
-    }
+pub struct AllEpoc {
+    pub epoces: Vec<EpocId>,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
@@ -165,6 +86,7 @@ pub struct PoolInfo {
     pub reward_account: CanonicalAddr,
 }
 
+//TODO : remove this.
 impl Default for PoolInfo {
     fn default() -> Self {
         Self {
@@ -202,12 +124,20 @@ pub fn balances_read<S: ReadonlyStorage>(storage: &S) -> ReadonlyBucket<S, Uint1
     bucket_read(BALANCE, storage)
 }
 
-pub fn token_state<S: Storage>(storage: &mut S) -> Singleton<S, TokenState> {
-    singleton(storage, TOKEN_STATE_KEY)
+pub fn save_epoc<S: Storage>(storage: &mut S) -> Singleton<S, EpocId> {
+    singleton(storage, EPOC_ID)
 }
 
-pub fn token_state_read<S: ReadonlyStorage>(storage: &S) -> ReadonlySingleton<S, TokenState> {
-    singleton_read(storage, TOKEN_STATE_KEY)
+pub fn epoc_read<S: ReadonlyStorage>(storage: &S) -> ReadonlySingleton<S, EpocId> {
+    singleton_read(storage, EPOC_ID)
+}
+
+pub fn save_all_epoc<S: Storage>(storage: &mut S) -> Singleton<S, AllEpoc> {
+    singleton(storage, ALL_EPOC_ID)
+}
+
+pub fn read_all_epocs<S: ReadonlyStorage>(storage: &S) -> ReadonlySingleton<S, AllEpoc> {
+    singleton_read(storage, ALL_EPOC_ID)
 }
 
 pub fn pool_info<S: Storage>(storage: &mut S) -> Singleton<S, PoolInfo> {
@@ -226,37 +156,43 @@ pub fn claim_read<S: ReadonlyStorage>(storage: &S) -> ReadonlyBucket<S, Uint128>
     bucket_read(PREFIX_REWARD, storage)
 }
 
-//this stores unboned amount in the storage.
-pub fn store_total_amount<S: Storage>(storage:&mut S, epoc_Id: Uint128, claimed: Uint128) -> StdResult<()> {
-    let vec = epoc_Id.0.to_be_bytes().to_vec();
+//this stores unboned amount in the storage per each epoc.
+pub fn store_total_amount<S: Storage>(
+    storage: &mut S,
+    epoc_id: u64,
+    claimed: Uint128,
+) -> StdResult<()> {
+    let vec = to_vec(&epoc_id)?;
     let value: Vec<u8> = to_vec(&claimed)?;
-    PrefixedStorage::new(PREFIX_UNBOUND_PER_EPOC, storage).set(vec.as_slice(), &value );
+    PrefixedStorage::new(PREFIX_UNBOUND_PER_EPOC, storage).set(&vec, &value);
     Ok(())
 }
 
-pub fn read_total_amount<S: Storage>(
-    storage: &S,
-    epoc_id: Uint128
-)-> StdResult<Uint128>{
-    let vec = epoc_id.0.to_be_bytes().to_vec();
-    let res = ReadonlyPrefixedStorage::new(PREFIX_UNBOUND_PER_EPOC, storage).get(vec.as_slice());
+pub fn read_total_amount<S: Storage>(storage: &S, epoc_id: u64) -> StdResult<Uint128> {
+    let vec = to_vec(&epoc_id)?;
+    let res = ReadonlyPrefixedStorage::new(PREFIX_UNBOUND_PER_EPOC, storage).get(&vec);
     match res {
-        Some(data)=> from_slice(&data),
+        Some(data) => from_slice(&data),
         None => Err(StdError::generic_err("no unbond amount is found")),
     }
 }
 
-pub fn store_delegation_map <S:Storage> (storage: &mut S, validator_address: HumanAddr, amount: Uint128)->StdResult<()>{
+// maps address of validator address to amount that the contract has delegated to
+pub fn store_delegation_map<S: Storage>(
+    storage: &mut S,
+    validator_address: HumanAddr,
+    amount: Uint128,
+) -> StdResult<()> {
     let vec = validator_address.0.as_bytes();
     let value = amount.0.to_be_bytes().to_vec();
     PrefixedStorage::new(PREFIX_DELEGATION_MAP, storage).set(vec, &value);
     Ok(())
 }
 
-pub fn read_delegation_map <S: Storage> (
+pub fn read_delegation_map<S: Storage>(
     storage: &S,
-    validator_address: HumanAddr
-)-> StdResult<Uint128> {
+    validator_address: HumanAddr,
+) -> StdResult<Uint128> {
     let vec = validator_address.0.as_bytes();
     let res = ReadonlyPrefixedStorage::new(PREFIX_DELEGATION_MAP, storage).get(vec);
     match res {
@@ -265,17 +201,32 @@ pub fn read_delegation_map <S: Storage> (
     }
 }
 
-pub fn store_holder_map <S: Storage> (storage: &mut S, holder_address: HumanAddr, index: Decimal)->StdResult<()>{
+// Returns all validators and their delegated amount
+pub fn read_validators<S: Storage>(storage: &S) -> StdResult<Vec<HumanAddr>> {
+    let mut validators: Vec<HumanAddr> = Vec::new();
+    let res = ReadonlyPrefixedStorage::new(PREFIX_DELEGATION_MAP, storage);
+    res.range(None, None, Order::Ascending).map(|item| {
+        let (key, _) = item;
+        let sender: HumanAddr = from_slice(&key).unwrap();
+        validators.push(sender);
+    });
+
+    Ok(validators)
+}
+
+// This is similar to HashMap<holder's address, reward_index>
+pub fn store_holder_map<S: Storage>(
+    storage: &mut S,
+    holder_address: HumanAddr,
+    index: Decimal,
+) -> StdResult<()> {
     let vec = holder_address.0.as_bytes();
-    let value:Vec<u8> = to_vec(&vec)?;
+    let value: Vec<u8> = to_vec(&vec)?;
     PrefixedStorage::new(PREFIX_HOLDER_MAP, storage).set(vec, &value);
     Ok(())
 }
 
-pub fn read_holder_map <S: Storage> (
-    storage: &S,
-    holder_address: HumanAddr
-) -> StdResult<Uint128> {
+pub fn read_holder_map<S: Storage>(storage: &S, holder_address: HumanAddr) -> StdResult<Decimal> {
     let vec = holder_address.0.as_bytes();
     let res = ReadonlyPrefixedStorage::new(PREFIX_HOLDER_MAP, storage).get(vec);
     match res {
@@ -284,19 +235,61 @@ pub fn read_holder_map <S: Storage> (
     }
 }
 
-pub fn store_undelegated_wait_list<'a, S: Storage> (storage: &'a mut S, epoc_id: Uint128, sender_address: HumanAddr, amount: Uint128)->StdResult<()>{
-    let vec = epoc_Id.0.to_be_bytes().to_vec();
+// Returns a HashMap of holders. <holders, reward_index>
+pub fn read_holders<'a, S: Storage>(storage: &'a S) -> StdResult<HashMap<HumanAddr, Decimal>> {
+    let mut holders: HashMap<HumanAddr, Decimal> = HashMap::new();
+    let res = ReadonlyPrefixedStorage::new(PREFIX_HOLDER_MAP, storage);
+    res.range(None, None, Order::Ascending).map(|item| {
+        let (key, value) = item;
+        let sender: HumanAddr = from_slice(&key).unwrap();
+        let index: Decimal = from_slice(&value).unwrap();
+        holders.insert(sender, index);
+    });
+
+    Ok(holders)
+}
+
+//stores undelegation wait list per each epoc.
+pub fn store_undelegated_wait_list<'a, S: Storage>(
+    storage: &'a mut S,
+    epoc_id: u64,
+    sender_address: HumanAddr,
+    amount: Uint128,
+) -> StdResult<()> {
+    let vec = to_vec(&epoc_id)?;
     let addr = sender_address.0.as_bytes();
     let mut position_indexer: Bucket<'a, S, Uint128> =
-        Bucket::multilevel(&[PREFIX_WAIT_MAP, vec], storage);
-    position_indexer.save(&addr, &amount )?;
+        Bucket::multilevel(&[PREFIX_WAIT_MAP, &vec], storage);
+    position_indexer.save(&addr, &amount)?;
 
     Ok(())
 }
 
-pub fn read_undelegated_wait_list <'a, S: ReadonlyStorage>(storage: &'a S, epoc_id: Uint128, sender_addr: HumanAddr)-> StdResult<Uint128> {
-    let vec = epoc_Id.0.to_be_bytes().to_vec();
-    let res: ReadonlyBucket<'a, S, Uint128 > = ReadonlyBucket::multilevel(&[PREFIX_WAIT_MAP, vec], storage);
+pub fn read_undelegated_wait_list<'a, S: ReadonlyStorage>(
+    storage: &'a S,
+    epoc_id: u64,
+    sender_addr: HumanAddr,
+) -> StdResult<Uint128> {
+    let vec = to_vec(&epoc_id)?;
+    let res: ReadonlyBucket<'a, S, Uint128> =
+        ReadonlyBucket::multilevel(&[PREFIX_WAIT_MAP, &vec], storage);
     let amount = res.load(sender_addr.0.as_bytes());
     amount
+}
+
+pub fn read_undelegated_wait_list_for_epoc<'a, S: ReadonlyStorage>(
+    storage: &'a S,
+    epoc_id: u64,
+) -> StdResult<HashMap<HumanAddr, Uint128>> {
+    let vec = to_vec(&epoc_id)?;
+    let mut list: HashMap<HumanAddr, Uint128> = HashMap::new();
+    let res: ReadonlyBucket<'a, S, Uint128> =
+        ReadonlyBucket::multilevel(&[PREFIX_WAIT_MAP, &vec], storage);
+
+    res.range(None, None, Order::Ascending).map(|item| {
+        let (k, v) = item.unwrap();
+        let key: HumanAddr = from_slice(&k).unwrap();
+        list.insert(key, v)
+    });
+    Ok(list)
 }
