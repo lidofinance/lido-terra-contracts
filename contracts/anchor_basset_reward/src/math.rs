eb42b23d63a3c3b525bff12b9f9462af0f6ea982
MSNTCS <najafi@codechain.io>
2021-01-13 22:55:15 +0900

    Update wasm files

--- contracts/anchor_basset_reward/src/math.rs
+++ contracts/anchor_basset_reward/src/math.rs
@@ -1,6 +1,6 @@
 use cosmwasm_std::{Decimal, Uint128};
 
-const DECIMAL_FRACTIONAL: Uint128 = Uint128(1_000_000_000_000u128);
+const DECIMAL_FRACTIONAL: Uint128 = Uint128(1_000_000_000u128);
 
 /// return a * b
 pub fn decimal_multiplication(a: Decimal, b: Decimal) -> Decimal {
