b74b7666a6901d424f1c456dbe5c43518ad38f38
pr0n00gler <programmer10110@yandex.ru>
2021-03-12 13:02:26 +0300

    config with hub_contract for validators_registry

--- contracts/validators-registry/src/registry.rs
+++ contracts/validators-registry/src/registry.rs
@@ -3,13 +3,29 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-use cosmwasm_std::Storage;
+use cosmwasm_std::{CanonicalAddr, Storage};
 use cosmwasm_std::{HumanAddr, Uint128};
-use cosmwasm_storage::{bucket, bucket_read, Bucket, ReadonlyBucket};
+use cosmwasm_storage::{
+    bucket, bucket_read, singleton, singleton_read, Bucket, ReadonlyBucket, ReadonlySingleton,
+    Singleton,
+};
 
 pub static CONFIG_KEY: &[u8] = b"config";
 
 pub static REGISTRY_KEY: &[u8] = b"validators_registry";
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct Config {
+    pub hub_contract: CanonicalAddr,
+}
+
+pub fn config<S: Storage>(storage: &mut S) -> Singleton<S, Config> {
+    singleton(storage, CONFIG_KEY)
+}
+
+pub fn config_read<S: Storage>(storage: &S) -> ReadonlySingleton<S, Config> {
+    singleton_read(storage, CONFIG_KEY)
+}
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct Validator {
