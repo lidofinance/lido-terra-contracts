84ca07da41f3afc1079158893ff52a3cf808a1cc
pr0n00gler <programmer10110@yandex.ru>
2021-08-19 16:31:23 +0300

    upgrade bluna_token_contract to cosmwasm 0.16

--- contracts/anchor_basset_token/src/lib.rs
+++ contracts/anchor_basset_token/src/lib.rs
@@ -3,10 +3,7 @@ pub mod msg;
 pub mod state;
 
 mod handler;
-pub mod querier;
+mod querier;
 
 #[cfg(test)]
 mod testing;
-
-#[cfg(target_arch = "wasm32")]
-cosmwasm_std::create_entry_points_with_migration!(contract);
--- contracts/anchor_basset_token/src/querier.rs
+++ contracts/anchor_basset_token/src/querier.rs
@@ -1,90 +1,36 @@
-use cosmwasm_std::{
-    from_binary, Api, Binary, Decimal, Extern, HumanAddr, Querier, QueryRequest, StdResult,
-    Storage, WasmQuery,
-};
+use cosmwasm_std::{Addr, Binary, DepsMut, QueryRequest, StdResult, WasmQuery};
 use cosmwasm_storage::to_length_prefixed;
 
 use crate::state::read_hub_contract;
-
 use anchor_basset_rewards_dispatcher::state::Config as RewardsDispatcherConfig;
+use basset::hub::Config;
 
-use hub_querier::{Config as HubConfig, State};
-
-pub fn query_reward_contract<S: Storage, A: Api, Q: Querier>(
-    deps: &Extern<S, A, Q>,
-) -> StdResult<HumanAddr> {
+pub fn query_reward_contract(deps: &DepsMut) -> StdResult<Addr> {
     let hub_address = deps
         .api
-        .human_address(&read_hub_contract(&deps.storage).unwrap())
+        .addr_humanize(&read_hub_contract(deps.storage).unwrap())
         .unwrap();
 
-    let res: Binary = deps.querier.query(&QueryRequest::Wasm(WasmQuery::Raw {
-        contract_addr: hub_address,
+    let config: Config = deps.querier.query(&QueryRequest::Wasm(WasmQuery::Raw {
+        contract_addr: hub_address.to_string(),
         key: Binary::from(to_length_prefixed(b"config")),
     }))?;
 
-    let hub_config: HubConfig = from_binary(&res)?;
-    let rewards_dispatcher_address = deps
+    let rewards_dispatcher_address = deps.api.addr_humanize(
+        &config
+            .reward_dispatcher_contract
+            .expect("the rewards dispatcher contract must have been registered"),
+    )?;
+
+    let rewards_dispatcher_config: RewardsDispatcherConfig =
+        deps.querier.query(&QueryRequest::Wasm(WasmQuery::Raw {
+            contract_addr: rewards_dispatcher_address.to_string(),
+            key: Binary::from(to_length_prefixed(b"config")),
+        }))?;
+
+    let bluna_reward_address = deps
         .api
-        .human_address(
-            &hub_config
-                .reward_dispatcher_contract
-                .expect("the rewards dispatcher contract must have been registered"),
-        )
-        .unwrap();
+        .addr_humanize(&rewards_dispatcher_config.bluna_reward_contract)?;
 
-    let res: Binary = deps.querier.query(&QueryRequest::Wasm(WasmQuery::Raw {
-        contract_addr: rewards_dispatcher_address,
-        key: Binary::from(to_length_prefixed(b"config")),
-    }))?;
-
-    let rewards_dispatcher_config: RewardsDispatcherConfig = from_binary(&res)?;
-    let rewards_address = deps
-        .api
-        .human_address(&rewards_dispatcher_config.bluna_reward_contract)
-        .unwrap();
-    Ok(rewards_address)
-}
-
-pub fn query_exchange_rates<S: Storage, A: Api, Q: Querier>(
-    deps: &Extern<S, A, Q>,
-) -> StdResult<(Decimal, Decimal)> {
-    let hub_address = deps
-        .api
-        .human_address(&read_hub_contract(&deps.storage).unwrap())
-        .unwrap();
-
-    let res: Binary = deps.querier.query(&QueryRequest::Wasm(WasmQuery::Raw {
-        contract_addr: hub_address,
-        key: Binary::from(to_length_prefixed(b"state")),
-    }))?;
-
-    let config: State = from_binary(&res)?;
-
-    Ok((config.bluna_exchange_rate, config.stluna_exchange_rate))
-}
-
-pub fn query_stluna_contract<S: Storage, A: Api, Q: Querier>(
-    deps: &Extern<S, A, Q>,
-) -> StdResult<HumanAddr> {
-    let hub_address = deps
-        .api
-        .human_address(&read_hub_contract(&deps.storage).unwrap())
-        .unwrap();
-
-    let res: Binary = deps.querier.query(&QueryRequest::Wasm(WasmQuery::Raw {
-        contract_addr: hub_address,
-        key: Binary::from(to_length_prefixed(b"config")),
-    }))?;
-
-    let config: HubConfig = from_binary(&res)?;
-    let address = deps
-        .api
-        .human_address(
-            &config
-                .stluna_token_contract
-                .expect("the stluna token contract must have been registered"),
-        )
-        .unwrap();
-    Ok(address)
+    Ok(bluna_reward_address)
 }
