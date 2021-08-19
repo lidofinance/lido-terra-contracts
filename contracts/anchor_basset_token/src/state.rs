b9c5f8e36be4d664efb3be8f8677161b46091417
pr0n00gler <programmer10110@yandex.ru>
2021-08-19 16:31:23 +0300

    upgrade bluna_token_contract to cosmwasm 0.16

--- contracts/anchor_basset_token/src/state.rs
+++ contracts/anchor_basset_token/src/state.rs
@@ -1,16 +1,50 @@
-use cosmwasm_std::{CanonicalAddr, ReadonlyStorage, StdResult, Storage};
-use cosmwasm_storage::{singleton, singleton_read};
+use cosmwasm_std::{CanonicalAddr, StdResult, Storage};
+//use cosmwasm_storage::{singleton, singleton_read};
+use cw_storage_plus::Item;
 
-const HUB_CONTRACT_KEY: &[u8] = b"hub_contract";
+pub const HUB_CONTRACT_KEY: Item<CanonicalAddr> = Item::new("\u{0}\u{c}hub_contract");
 
 // meta is the token definition as well as the total_supply
-pub fn read_hub_contract<S: ReadonlyStorage>(storage: &S) -> StdResult<CanonicalAddr> {
-    singleton_read(storage, HUB_CONTRACT_KEY).load()
+pub fn read_hub_contract(storage: &dyn Storage) -> StdResult<CanonicalAddr> {
+    HUB_CONTRACT_KEY.load(storage)
 }
 
-pub fn store_hub_contract<S: Storage>(
-    storage: &mut S,
+pub fn store_hub_contract(
+    storage: &mut dyn Storage,
     hub_contract: &CanonicalAddr,
 ) -> StdResult<()> {
-    singleton(storage, HUB_CONTRACT_KEY).save(hub_contract)
+    HUB_CONTRACT_KEY.save(storage, hub_contract)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    use cosmwasm_std::testing::mock_dependencies;
+    use cosmwasm_std::{Api, StdResult, Storage};
+    use cosmwasm_storage::{singleton, singleton_read};
+
+    pub static HUB_CONTRACT: &[u8] = b"hub_contract";
+
+    pub fn store_hub(storage: &mut dyn Storage, params: &CanonicalAddr) -> StdResult<()> {
+        singleton(storage, HUB_CONTRACT).save(params)
+    }
+    pub fn read_hub(storage: &dyn Storage) -> StdResult<CanonicalAddr> {
+        singleton_read(storage, HUB_CONTRACT).load()
+    }
+
+    #[test]
+    fn hub_legacy_compatibility() {
+        let mut deps = mock_dependencies(&[]);
+        store_hub(
+            &mut deps.storage,
+            &deps.api.addr_canonicalize("hub").unwrap(),
+        )
+        .unwrap();
+
+        assert_eq!(
+            HUB_CONTRACT_KEY.load(&deps.storage).unwrap(),
+            read_hub(&deps.storage).unwrap()
+        );
+    }
 }
