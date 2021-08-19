38abafbdc0506a41f1bf040536bf080e8540a278
pr0n00gler <programmer10110@yandex.ru>
2021-08-19 17:06:40 +0300

    upgrade stluna_token_contract to cosmwasm 0.16

--- contracts/anchor_basset_token_stluna/src/msg.rs
+++ contracts/anchor_basset_token_stluna/src/msg.rs
@@ -1,5 +1,5 @@
-use cosmwasm_std::HumanAddr;
-use cw20::{Cw20CoinHuman, MinterResponse};
+use cw20::{Cw20Coin, MinterResponse};
+use cw20_base::msg::InstantiateMarketingInfo;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
@@ -8,7 +8,8 @@ pub struct TokenInitMsg {
     pub name: String,
     pub symbol: String,
     pub decimals: u8,
-    pub initial_balances: Vec<Cw20CoinHuman>,
+    pub initial_balances: Vec<Cw20Coin>,
     pub mint: Option<MinterResponse>,
-    pub hub_contract: HumanAddr,
+    pub hub_contract: String,
+    pub marketing: Option<InstantiateMarketingInfo>,
 }
--- contracts/anchor_basset_token_stluna/src/state.rs
+++ contracts/anchor_basset_token_stluna/src/state.rs
@@ -1,15 +1,15 @@
-use cosmwasm_std::{CanonicalAddr, ReadonlyStorage, StdResult, Storage};
+use cosmwasm_std::{CanonicalAddr, StdResult, Storage};
 use cosmwasm_storage::{singleton, singleton_read};
 
 const HUB_CONTRACT_KEY: &[u8] = b"hub_contract";
 
 // meta is the token definition as well as the total_supply
-pub fn read_hub_contract<S: ReadonlyStorage>(storage: &S) -> StdResult<CanonicalAddr> {
+pub fn read_hub_contract(storage: &dyn Storage) -> StdResult<CanonicalAddr> {
     singleton_read(storage, HUB_CONTRACT_KEY).load()
 }
 
-pub fn store_hub_contract<S: Storage>(
-    storage: &mut S,
+pub fn store_hub_contract(
+    storage: &mut dyn Storage,
     hub_contract: &CanonicalAddr,
 ) -> StdResult<()> {
     singleton(storage, HUB_CONTRACT_KEY).save(hub_contract)
