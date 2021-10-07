c0d53f724fd9bea24af581499b240c573ce21d5d
pakgw <pakgw@xplanet.ru>
2021-10-07 14:39:40 +0300

    fixed lido licences again

--- contracts/anchor_basset_validators_registry/src/registry.rs
+++ contracts/anchor_basset_validators_registry/src/registry.rs
@@ -1,5 +1,6 @@
-// Copyright 2021 Lido
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Copyright 2021 Anchor Protocol. Modified by Lido
+//
+// Licensedicensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
