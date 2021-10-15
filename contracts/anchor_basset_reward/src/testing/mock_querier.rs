5948af1b134e389bd3a0d4c3822f5faac865e07d
Andrei Zavgorodnii <andrei.z@p2p.org>
2021-10-15 17:52:35 +0300

    fixed tests

--- contracts/anchor_basset_reward/src/testing/mock_querier.rs
+++ contracts/anchor_basset_reward/src/testing/mock_querier.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use basset::hub::{Config};
+use basset::hub::Config;
 use cosmwasm_std::testing::{MockApi, MockQuerier, MockStorage, MOCK_CONTRACT_ADDR};
 use cosmwasm_std::{
     from_slice, to_binary, Api, Coin, ContractResult, Decimal, OwnedDeps, Querier, QuerierResult,
