8225344a034e40015884b7521efca0e9b2298151
pr0n00gler <programmer10110@yandex.ru>
2021-10-19 15:57:23 +0300

    fix: simplify fee calculation #LSC-141

--- packages/basset/src/tax_querier.rs
+++ packages/basset/src/tax_querier.rs
@@ -26,8 +26,5 @@ pub fn deduct_tax(querier: &QuerierWrapper, coin: Coin) -> StdResult<Coin> {
 }
 
 pub fn compute_lido_fee(amount: Uint128, fee_rate: Decimal) -> StdResult<Uint128> {
-    Ok(amount.checked_sub(amount.multiply_ratio(
-        DECIMAL_FRACTION,
-        DECIMAL_FRACTION * fee_rate + DECIMAL_FRACTION,
-    ))?)
+    Ok(amount * fee_rate)
 }
