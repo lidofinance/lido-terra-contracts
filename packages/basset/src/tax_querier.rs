e31fddedeb2e67b805f71a50b80837c5e0f35e3d
Mike Mozhaev <programmer10110@yandex.ru>
2021-10-28 15:00:00 +0300

    Fix: simplify fee calculation (#89)
    * fix: simplify fee calculation #LSC-141

--- packages/basset/src/tax_querier.rs
+++ packages/basset/src/tax_querier.rs
@@ -26,8 +26,47 @@ pub fn deduct_tax(querier: &QuerierWrapper, coin: Coin) -> StdResult<Coin> {
 }
 
 pub fn compute_lido_fee(amount: Uint128, fee_rate: Decimal) -> StdResult<Uint128> {
-    Ok(amount.checked_sub(amount.multiply_ratio(
-        DECIMAL_FRACTION,
-        DECIMAL_FRACTION * fee_rate + DECIMAL_FRACTION,
-    ))?)
+    Ok(amount * fee_rate)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::str::FromStr;
+
+    #[test]
+    fn test_compute_lido_fee() {
+        struct TestCase {
+            fee_rate: Decimal,
+            value: Uint128,
+            expected_fee_value: Uint128,
+        }
+
+        let test_cases: Vec<TestCase> = vec![
+            TestCase {
+                fee_rate: Decimal::from_str("0.05").unwrap(),
+                value: Uint128::from(100u128),
+                expected_fee_value: Uint128::from(5u128),
+            },
+            TestCase {
+                fee_rate: Decimal::from_str("0.0").unwrap(),
+                value: Uint128::from(100u128),
+                expected_fee_value: Uint128::from(0u128),
+            },
+            TestCase {
+                fee_rate: Decimal::from_str("0.033").unwrap(),
+                value: Uint128::from(500u128),
+                expected_fee_value: Uint128::from(16u128),
+            },
+            TestCase {
+                fee_rate: Decimal::from_str("0.04").unwrap(),
+                value: Uint128::from(234274526u128),
+                expected_fee_value: Uint128::from(9370981u128),
+            },
+        ];
+        for test_case in test_cases {
+            let actual_fee = compute_lido_fee(test_case.value, test_case.fee_rate).unwrap();
+            assert_eq!(actual_fee, test_case.expected_fee_value);
+        }
+    }
 }
